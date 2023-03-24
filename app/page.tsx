ac64eeec6d71d0e4cf5d2bff197a743f7f32125d
makstyle119 <moizprince786@gmail.com>
2023-03-25 02:18:21 +0500

    basic zakat app

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,55 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client';
+import { useState } from 'react';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	const [income, setIncome] = useState<number>(0);
+	const [debt, setDebt] = useState<number>(0);
+	const getYear = new Date().getFullYear();
+	const [output, setOutput] = useState<number>(0);
+	const calculateZakat = (): void => {
+		if (income > 0) {
+			const zakat: number = (2.5 / 100) * (income - debt);
+			setOutput(zakat);
+		} else {
+			setOutput(0);
+		}
+	};
+	return (
+		<>
+			<h1>Zakat Calculator App</h1>
+			<form>
+				<label htmlFor='income'>Total Income:</label>
+				<input
+					value={income}
+					type='number'
+					id='income'
+					name='income'
+					onChange={(e) => setIncome(Number(e?.target?.value))}
+					onKeyUpCapture={() => calculateZakat()}
+					required
+				/>
+				<br />
+				<label htmlFor='debt'>Total Debt:</label>
+				<input
+					value={debt}
+					type='number'
+					id='debt'
+					name='debt'
+					onChange={(e) => setDebt(Number(e?.target?.value))}
+					onKeyUpCapture={() => calculateZakat()}
+					required
+				/>
+			</form>
+			<div id='output'>Your Zakat is: {output}</div>
+			<footer>
+				<p>
+					&copy; {getYear} Zakat Calculator App. DEVELOP & MAINTAIN BY MAK
+					DEVELOPMENT HOUSE.
+				</p>
+			</footer>
+		</>
+	);
 }
